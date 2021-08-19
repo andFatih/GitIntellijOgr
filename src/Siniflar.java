5c78ff178ed7642679150b973b43c8063bdb6e59
ismettechno <ismettechno@gmail.com>
2021-08-19 20:18:52 +0100

    Gun 03: Sınıflar 2

--- src/Siniflar.java
+++ src/Siniflar.java
@@ -1,5 +1,7 @@
 public class Siniflar {
     public static void main(String[] args) {
         System.out.println("Sınıflar oluşturuldu");
+
+        System.out.println(" yeni değişiklik ");
     }
 }
